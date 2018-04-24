79f2cf875814148e5d8b1d53f70166cf94d289e3
guli <guli@tsinghua.org.cn>
2018-04-24 20:03:25 +0800

    base

--- priv/bgx.sol
+++ priv/bgx.sol
@@ -193,6 +193,7 @@ contract BGCToken {
     }
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(!isInLockStage);
         require(_value <= allowance[_from][msg.sender]);     // Check allowance
         allowance[_from][msg.sender] -= _value;
         _transfer(_from, _to, _value);
