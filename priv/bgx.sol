7107acd341f598cf750fdc9ba31e8178ecb4dd53
guli <guli@tsinghua.org.cn>
2018-04-25 19:19:53 +0800

    base: new sol logic

--- priv/bgx.sol
+++ priv/bgx.sol
@@ -7,62 +7,78 @@ contract BGCToken {
     string public name = "BitGameCoin1";
     string public symbol = "BGC1";
 
-    uint256 baseTokenPerETH = 50000; // 无优惠时的兑换比率
-
     address ethFundAddress  = $ETH_FUND_ADDR; //以太坊轉存地址
-    address[] foundationAddresses = [$FOUNDATION_ADDRS]; // 基金会地址，15个
-    address[] teamAddresses = [$TEAM_ADDRS]; // 团队地址，15个
+    address[] foundationAddresses = [$FOUNDATION_ADDRS]; // 基金会地址，7个
+    address[] teamAddresses = [$TEAM_ADDRS]; // 团队地址，1个
     address[] miningAddresses = [$MINING_ADDRS]; // 挖矿地址，2个
-    address[] cornerstoneAddresses = [$CORNERSTONE_ADDRS]; // 基石私募地址，10个
-    address[] preIcoAddresses = [$PREICO_ADDRS]; // PreICO地址，5个
+    address[] angelAddresses = [$ANGEL_ADDRS]; // 天使地址，1个
+    address[] cornerstoneAddresses = [$CORNERSTONE_ADDRS]; // 基石地址，7个
+    address[] preIcoAddresses = [$PREICO_ADDRS]; // PreICO地址，1个
+
+    //address ethFundAddress  = 0xC817a2afe8F6ba1a697dAaA1df834e18Be9403e7; //以太坊轉存地址
+    //address[] foundationAddresses = [0xBc9d9A1EE11dC2803BA2daaAa892483052Ed97f5,
+    //                                 0x3CA55e25C110b175B6622586aC31a6682a916670,
+    //                                 0xF13b2d7c5d6d6E93f79D41FA72cFD33A75c0607d,
+    //                                 0xc321Cf1D0ab11743cB5dDB4d77F6Ede8a08D3281,
+    //                                 0xf7179c8A47F511E4fcAcA9b6187ED4052cBBB7BB,
+    //                                 0x516c06F2A390E62c2F6cB3C2E38c5c6dF5A17141,
+    //                                 0xE71a86f7FFa3E3aA92e5cA6b6df8B56d8600c7D9]; // 基金会地址，7个
+    //address[] teamAddresses = [0x3CFdEC9041b04a7eEb07a732B964a5B33f9Ebe1F]; // 团队地址，1个
+    //address[] miningAddresses = [0x710967a31D79BCFBF053292aB21Bbc559e288407,
+    //                             0x7bF52Ef4b6e8bEeB24c4Dea4c8e94177739561a0]; // 挖矿地址，2个
+    //address[] angelAddresses = [0x122399734D64d6c4aa46b85959A3304CA812161f]; // 天使地址，1个
+    //address[] cornerstoneAddresses = [0x9d35F83982A479F611fa893452f6876972Ec6348,
+    //                                  0x5CcB5458f48c2f70a458004d2e589Bc8bAeC6aA0,
+    //                                  0x377221D5b7776C1Ba4B8e8d11a32CF9a7469A095,
+    //                                  0xc4381bc9dDFaa8A9954CF2615F80F8Fc145E024F,
+    //                                  0x699a3be17F729F3eB965fBb7d71Db185016B1215,
+    //                                  0x9F793B134E41Bb404142B598E05Ea6ed5477D392,
+    //                                  0xA7FF388DAfD240505f9a1d3ca37c15E058B9D4ea]; // 基石地址，7个
+    //address[] preIcoAddresses = [0x4d1Ffd49d47552adcaf1729b9C4A2320419b81E1]; // PreICO地址，1个
 
     uint256 startTime = $START_TIME; // 开始时间戳，UTC-0
-    uint256 stage1Seconds = 120; // 10% bonus
-    uint256 stage2Seconds = 120; // 5% bonus
-    uint256 stage3Seconds = 120; // 0 bonus
-    uint256 stage4Seconds = 120; // -5% bonus
-    uint256 stage5Seconds = 120; // -10% bonus
+    uint256 endTime = $END_TIME; // 结束时间戳，UTC-0
+    uint256 lockEndTime = $LOCK_END_TIME; // 锁定结束时间戳，UTC-0
 
-    //uint256 startTime = 1525017600; // 开始时间戳，2018/4/30 0:0:0 UTC-0
-    //uint256 stage1Seconds = 3 * 24 * 3600; // 10% bonus，4.30 - 5.2
-    //uint256 stage2Seconds = 4 * 24 * 3600; // 5% bonus，5.3 - 5.6
-    //uint256 stage3Seconds = 10 * 24 * 3600; // 0 bonus，5.7 - 5.16
-    //uint256 stage4Seconds = 8 * 24 * 3600; // -5% bonus，5.17 - 5.24
-    //uint256 stage5Seconds = 6 * 24 * 3600; // -10% bonus，5.25 - 5.30
+    //uint256 startTime = 1525708800; // 开始时间戳，2018/5/8 0:0:0 UTC-0
+    //uint256 endTime = 1528473600; // 结束时间戳，2018/6/9 0:0:0 UTC-0
+    //uint256 lockEndTime = 1528473600; // 锁定结束时间戳，2018/6/9 0:0:0 UTC-0
     // <====== 正式发布需要修正的参数
-
-
-    uint256 public stage1EndTime = startTime + stage1Seconds;
-    uint256 public stage2EndTime = stage1EndTime + stage2Seconds;
-    uint256 public stage3EndTime = stage2EndTime + stage3Seconds;
-    uint256 public stage4EndTime = stage3EndTime + stage4Seconds;
-    uint256 public endTime = stage4EndTime + stage5Seconds; // 结束时间戳
 
     uint256 public decimals = 18;
     uint256 DECIMALSFACTOR = 10 ** decimals;
     uint256 constant weiDECIMALS = 18; // 以太币的小数位
     uint256 weiFACTOR = 10 ** weiDECIMALS; // 以太币的单位换算值
 
-    uint256 reservedAmountPerFoundationAddress = 1 * (10**8) * DECIMALSFACTOR;// 轉移1亿
-    uint256 reservedAmountPerTeamAddress = 1 * (10**8) * DECIMALSFACTOR;// 轉移1亿
-    uint256 reservedAmountPerMiningAddress = 15 * (10**8) * DECIMALSFACTOR;// 轉移15亿
-    uint256 reservedAmountPerCornerstoneAddress = 1 * (10**8) * DECIMALSFACTOR;// 轉移1亿
-    uint256 reservedAmountPerPreIcoAddress = 1 * (10**8) * DECIMALSFACTOR;// 轉移1亿
-
-    uint256 public currentTokenPerETH = baseTokenPerETH;
+    uint256[] foundationAmounts = [5 * (10**8) * DECIMALSFACTOR,
+                                   5 * (10**8) * DECIMALSFACTOR,
+                                   1 * (10**8) * DECIMALSFACTOR,
+                                   1 * (10**8) * DECIMALSFACTOR,
+                                   1 * (10**8) * DECIMALSFACTOR,
+                                   1 * (10**8) * DECIMALSFACTOR,
+                                   1 * (10**8) * DECIMALSFACTOR];
+    uint256[] teamAmounts = [15 * (10**8) * DECIMALSFACTOR];
+    uint256[] miningAmounts = [15 * (10**8) * DECIMALSFACTOR,
+                               15 * (10**8) * DECIMALSFACTOR];
+    uint256[] angelAmounts = [5 * (10**8) * DECIMALSFACTOR];
+    uint256[] cornerstoneAmounts = [1 * (10**8) * DECIMALSFACTOR,
+                                    1 * (10**8) * DECIMALSFACTOR,
+                                    1 * (10**8) * DECIMALSFACTOR,
+                                    1 * (10**8) * DECIMALSFACTOR,
+                                    1 * (10**8) * DECIMALSFACTOR,
+                                    2 * (10**8) * DECIMALSFACTOR,
+                                    3 * (10**8) * DECIMALSFACTOR];
+    uint256[] preIcoAmounts = [5 * (10**8) * DECIMALSFACTOR];
 
     address contractOwner;
-    uint256 ethRaised; // 收到的ETH总数量，单位Wei
-    uint256 tokenDistributed; // 分发出去的Token数量
+    uint256 ethRaised = 0; // 收到的ETH总数量，单位Wei
     uint256 donationCount; // 参与的总次数
 
     uint256 public totalSupply = 100 * (10**8) * DECIMALSFACTOR; // 总量100亿
     uint256 public availableSupply = totalSupply; // 剩余的代币数量
-    uint256 softCap = 5.5 * (10**8) * DECIMALSFACTOR; // 软顶5.5亿代币
+    uint256 hardCap = 30000 * weiFACTOR; // 硬顶3万ETH
     uint256 minimumDonation = 1 * 10 ** (weiDECIMALS - 1); // 最低参与0.1ETH才能参与
 
-    uint8 public currentStage = 0;
-    bool public isInLockStage = true;
     bool public finalised = false;
 
     // 存储所有用户的代币余额值
@@ -81,27 +97,32 @@ contract BGCToken {
         // 采用累加方式，防止有地址重复
         uint i = 0;
         for (i = 0; i < foundationAddresses.length; i++){
-            balanceOf[foundationAddresses[i]] += reservedAmountPerFoundationAddress;
-            availableSupply -= reservedAmountPerFoundationAddress;
+            balanceOf[foundationAddresses[i]] += foundationAmounts[i];
+            availableSupply -= foundationAmounts[i];
         }
         for (i = 0; i < teamAddresses.length; i++){
-            balanceOf[teamAddresses[i]] += reservedAmountPerTeamAddress;
-            availableSupply -= reservedAmountPerTeamAddress;
+            balanceOf[teamAddresses[i]] += teamAmounts[i];
+            availableSupply -= teamAmounts[i];
         }
         for (i = 0; i < miningAddresses.length; i++){
-            balanceOf[miningAddresses[i]] += reservedAmountPerMiningAddress;
-            availableSupply -= reservedAmountPerMiningAddress;
+            balanceOf[miningAddresses[i]] += miningAmounts[i];
+            availableSupply -= miningAmounts[i];
+        }
+        for (i = 0; i < angelAddresses.length; i++){
+            balanceOf[angelAddresses[i]] += angelAmounts[i];
+            availableSupply -= angelAmounts[i];
         }
         for (i = 0; i < cornerstoneAddresses.length; i++){
-            balanceOf[cornerstoneAddresses[i]] += reservedAmountPerCornerstoneAddress;
-            availableSupply -= reservedAmountPerCornerstoneAddress;
+            balanceOf[cornerstoneAddresses[i]] += cornerstoneAmounts[i];
+            availableSupply -= cornerstoneAmounts[i];
         }
         for (i = 0; i < preIcoAddresses.length; i++){
-            balanceOf[preIcoAddresses[i]] += reservedAmountPerPreIcoAddress;
-            availableSupply -= reservedAmountPerPreIcoAddress;
+            balanceOf[preIcoAddresses[i]] += preIcoAmounts[i];
+            availableSupply -= preIcoAmounts[i];
         }
 
-        balanceOf[contractOwner] = availableSupply; // 剩下的代币初始都存在创建合约的地址上
+        // 剩下的代币初始都存在创建合约的地址上
+        balanceOf[contractOwner] = availableSupply;
     }
 
     // fallback方法，如果用户未在转账data中添加数据，默认是走这个方法
@@ -112,38 +133,17 @@ contract BGCToken {
         require(block.timestamp >= startTime);
         require(block.timestamp <= endTime);
 
-        // 判断代币是否已经分发完毕
-        require(availableSupply > 0);
+        // 判断硬顶
+        require(ethRaised < hardCap);
 
-        // 分配代币
-        require(msg.value >= minimumDonation); // 达到最低捐赠额度才能继续，否则失败
+        // 达到最低捐赠额度才能继续，否则失败
+        require(msg.value >= minimumDonation);
 
-        if (block.timestamp <= stage1EndTime){
-            currentStage = 1;
-            currentTokenPerETH = baseTokenPerETH + baseTokenPerETH / 10;
-        }else if (block.timestamp <= stage2EndTime){
-            currentStage = 2;
-            currentTokenPerETH = baseTokenPerETH + baseTokenPerETH / 20;
-        }else if (block.timestamp <= stage3EndTime){
-            currentStage = 3;
-            currentTokenPerETH = baseTokenPerETH;
-        }else if (block.timestamp <= stage4EndTime){
-            currentStage = 4;
-            currentTokenPerETH = baseTokenPerETH - baseTokenPerETH / 20;
-        }else{
-            currentStage = 5;
-            currentTokenPerETH = baseTokenPerETH - baseTokenPerETH / 10;
-        }
-
-        // 计算该地址本次参与所获得的Token数量
         uint256 etherValue = msg.value;
-        uint256 tokenValue = currentTokenPerETH * etherValue / 10 ** (weiDECIMALS - decimals);
 
-        // 边界条件，未超过部分的ETH正常计算相应代币，超过的部分退回给用户
-        if (tokenValue > availableSupply){
-            tokenValue = availableSupply;
-            // 单位为wei
-            etherValue = weiFACTOR * availableSupply / currentTokenPerETH / DECIMALSFACTOR;
+        // 边界条件，未超过部分的ETH正常收纳，超过的部分退回给用户
+        if (ethRaised + etherValue > hardCap){
+            etherValue = hardCap - ethRaised;
             // 超过的部分退回给用户
             assert(msg.value > etherValue);
             msg.sender.transfer(msg.value - etherValue);
@@ -152,18 +152,8 @@ contract BGCToken {
         // 转移ETH到指定ETH存币地址
         ethFundAddress.transfer(etherValue);
 
-        // 调用内部转账方法给该地址转相应数量的Token
-        _transfer(contractOwner, msg.sender, tokenValue);
-
         donationCount += 1;
         ethRaised += etherValue;
-        availableSupply -= tokenValue;
-        tokenDistributed += tokenValue;
-        if (tokenDistributed >= softCap){
-            isInLockStage = false;
-        }else{
-            isInLockStage = true;
-        }
     }
 
     /**
@@ -187,13 +177,48 @@ contract BGCToken {
         assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
     }
 
+    function _isInWhiteAddresses(address _from) internal view returns (bool success) {
+        uint i = 0;
+        for (i = 0; i < foundationAddresses.length; i++){
+            if (foundationAddresses[i] == _from){
+                return true;
+            }
+        }
+        for (i = 0; i < teamAddresses.length; i++){
+            if (teamAddresses[i] == _from){
+                return true;
+            }
+        }
+        for (i = 0; i < miningAddresses.length; i++){
+            if (miningAddresses[i] == _from){
+                return true;
+            }
+        }
+        for (i = 0; i < angelAddresses.length; i++){
+            if (angelAddresses[i] == _from){
+                return true;
+            }
+        }
+        for (i = 0; i < cornerstoneAddresses.length; i++){
+            if (cornerstoneAddresses[i] == _from){
+                return true;
+            }
+        }
+        for (i = 0; i < preIcoAddresses.length; i++){
+            if (preIcoAddresses[i] == _from){
+                return true;
+            }
+        }
+        return false;
+    }
+
     function transfer(address _to, uint256 _value) public {
-        require(!isInLockStage);
+        require(block.timestamp > lockEndTime || _isInWhiteAddresses(msg.sender));
         _transfer(msg.sender, _to, _value);
     }
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        require(!isInLockStage);
+        require(block.timestamp > lockEndTime || _isInWhiteAddresses(_from));
         require(_value <= allowance[_from][msg.sender]);     // Check allowance
         allowance[_from][msg.sender] -= _value;
         _transfer(_from, _to, _value);
@@ -214,6 +239,7 @@ contract BGCToken {
     }
 
     function burn(uint256 _value) public returns (bool success) {
+        require(block.timestamp > lockEndTime);
         require(balanceOf[msg.sender] >= _value);   // Check if the sender has enough
         balanceOf[msg.sender] -= _value;            // Subtract from the sender
         totalSupply -= _value;                      // Update totalSupply
@@ -222,6 +248,7 @@ contract BGCToken {
     }
 
     function burnFrom(address _from, uint256 _value) public returns (bool success) {
+        require(block.timestamp > lockEndTime);
         require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
         require(_value <= allowance[_from][msg.sender]);    // Check allowance
         balanceOf[_from] -= _value;                         // Subtract from the targeted balance
@@ -236,16 +263,5 @@ contract BGCToken {
         require(!finalised);
 
         finalised = true;
-    }
-
-    //手工解除代币锁定
-    function unlockTokens() public {
-        require(msg.sender == contractOwner);
-
-        isInLockStage = false;
-    }
-
-    function tokenHasDistributed() public constant returns (uint256) {
-        return tokenDistributed;
     }
 }
